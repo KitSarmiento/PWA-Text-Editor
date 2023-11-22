deb0090a16a74d34601a6fe27b488fb250340a00
KitSarmiento <ksarmiento020@gmail.com>
2023-11-21 20:39:29 -0500

    Added a HtmlWebpackPlugin and WebpackPwaManifest.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,74 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
+
+    // Used module 19 Activity 25 & 26 (Manifest) as a reference for the code.
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        filename: "JATE",
+      }),
+
+      new WebpackPwaManifest({
+        name: "JATE Text Editor",
+        short_name: "JATE",
+        description: "A text editor that can be accessed through a browser",
+        start_url: "/",
+        background_color: "#36454F",
+        theme_color: "#36454F",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/assets/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+
+      new InjectManifest({
+        swSrc: "./src/service-worker.js",
+        swDest: "service-worker.js",
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
